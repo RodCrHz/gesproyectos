225f0832786c452dab5e12d3620877db0cd949e7
momo_ouroboros <oalmejia@hotmail.com>
2015-10-06 00:15:42 -0600

    Prueba

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+// comentario rod
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
