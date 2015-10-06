e68dcf716160b2872c12e1b2337d9d46191f5ba0
Rodrigo Cruz <jrch93@hotmail.com>
2015-10-05 22:48:13 -0600

    solucinado errores en main...

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,7 +30,7 @@
 			'items'=>array(
 				array('label'=>'Inicio', 'url'=>array('/site/index')),
 				array('label'=>'Proyectos', 'url'=>array('/project/index','visible'=>!Yii::app()->user->isGuest)),
-				array('label'=>'Usuarios', 'url'=>array('/user/index','visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Usuarios', 'url'=>array('/user/index','visible'=>!Yii::app()->user->isGuest)),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
